9583ff195842ea63c34529aa8535803e1260a6cb
huangkai <18778335525@163.com>
2017-10-08 23:19:48 +0800

    增加密码修改和修改成功的视图以及模版

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login, logout, logout_then_login
+from django.contrib.auth.views import login, logout, logout_then_login, password_change, password_change_done
 
 urlpatterns = [
     # 登入登出
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
     # dashboard
     url(r'^$', views.dashboard, name='dashboard'),
+    # 修改密码
+    url(r'^password-change/$', password_change, name='password_change'),
+    url(r'^password-change/done/$', password_change_done, name='password_change_done'),
 ]
